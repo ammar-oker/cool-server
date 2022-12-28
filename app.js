65ccc6364064fca0d5f5a6fd069ec862cd29beb0
umurerman <115986150+umurerman@users.noreply.github.com>
2022-12-28 14:56:40 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const Express = require('express');
+
+const app = Express();
+const PORT = 3000;
+
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello, world!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.info('listing on port %s', PORT);
+})
