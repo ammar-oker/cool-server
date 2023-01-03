78bf7ad12f405f01c9abbd3f6892a869f13ea389
uaerman <34603648+uaerman@users.noreply.github.com>
2023-01-03 14:54:26 +0300

    Added rancher api for auto update

---
+++ release.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -u "$CATTLE_ACCESS_KEY:$CATTLE_SECRET_KEY" \
+-X POST \
+-H 'Accept: application/json' \
+-H 'Content-Type: application/json' \
+'https://rancher.rising.dev/v3/project/c-gsq6k:p-4fjxn/workloads/deployment:default:my-cool-server?action=redeploy'
