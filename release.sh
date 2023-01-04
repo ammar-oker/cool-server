3235bec440d1be960be541372458236d8b150348
Umurcan Erman <umurcan@risingdijital.com>
2023-01-04 11:52:31 +0000

    Update pipeline

--- release.sh
+++ release.sh
@@ -5,4 +5,4 @@ curl -u "$CATTLE_ACCESS_KEY:$CATTLE_SECRET_KEY" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
-'https://rancher.rising.dev/v3/project/c-gsq6k:p-4fjxn/workloads/deployment:default:my-cool-server?action=redeploy'
+'https://rancher.rising.dev/v3/project/c-wqz8t:p-tstns/workloads/deployment:default:my-cool-server?action=redeploy'
